41476b394abc1ad28a25954bedcdbef95dafcddd
Danilo Silva <contact@danilocgsilva.me>
2026-01-07 05:41:06 -0300

    Create environment validation

--- making_question_ai_decorator_storage/CheckEnvironmentCredentials.py
+++ making_question_ai_decorator_storage/CheckEnvironmentCredentials.py
@@ -1,17 +1,40 @@
 import os
 
 class CheckEnvironmentCredentials:
+    databaseUser = None
+    databasePassword = None
+    databaseHost = None
+    databaseName = None
+    databasePort = 3306
+    
     def __init__(self):
-        # self.required_vars = [
-        #     'MYSQL_USER',
-        #     'MYSQL_PASSWORD',
-        #     'MYSQL_HOST',
-        #     'MYSQL_PORT',
-        #     'MYSQL_DATABASE'
-        # ]
-        
-        # for var in self.required_vars:
-        #     if not os.getenv(var):
-        #         raise EnvironmentError(f"Environment variable {var} is not set.")
-        #     else:
-        #         self.
+        errors = []
+        if not os.getenv('MYSQL_USER'):
+            errors.append("Environment variable MYSQL_USER is not set.")
+        if not os.getenv('MYSQL_PASSWORD'):
+            errors.append("Environment variable MYSQL_PASSWORD is not set.")
+        if not os.getenv('MYSQL_HOST'):
+            errors.append("Environment variable MYSQL_HOST is not set.")
+        if not os.getenv('MYSQL_DATABASE'):
+            errors.append("Environment variable MYSQL_DATABASE is not set.")
+            
+    def is_valid(self):
+        return len(errors) == 0
+    
+    def get_errors_string(self):
+        return "\n".join(errors)
+    
+    def getDatabaseUser(self):
+        return self.databaseUser
+    
+    def getDatabasePassword(self):
+        return self.databasePassword
+    
+    def getDatabaseHost(self):
+        return self.databaseHost
+    
+    def getDatabaseName(self):
+        return self.databaseName
+    
+    def getDatabasePort(self):
+        return self.databasePort
--- making_question_ai_decorator_storage/DatabaseCredentials.py
+++ making_question_ai_decorator_storage/DatabaseCredentials.py
@@ -1,9 +1,24 @@
 import os
+from making_question_ai_decorator_storage.CheckEnvironmentCredentials import CheckEnvironmentCredentials
+from making_question_ai_decorator_storage.MissingConnectionCredentialsError import MissingConnectionCredentialsError
 
 class DatabaseCredentials:
+    user: str
+    password: str
+    host: str
+    port: int
+    database_name: str
+    
     def __init__(self):
-        self.user = os.getenv('MYSQL_USER')
-        self.password = os.getenv('MYSQL_PASSWORD')
-        self.host = os.getenv('MYSQL_HOST')
-        self.port = int(os.getenv('MYSQL_PORT', 3306))
-        self.database = os.getenv('MYSQL_DATABASE')
+        checkEnvironmentCredentials = CheckEnvironmentCredentials()
+        if not checkEnvironmentCredentials.is_valid():
+            raise MissingConnectionCredentialsError(
+                "Missing database connection credentials:\n" +
+                checkEnvironmentCredentials.get_errors_string()
+            )
+
+        self.user = checkEnvironmentCredentials.get_user()
+        self.password = checkEnvironmentCredentials.get_password()
+        self.host = checkEnvironmentCredentials.get_host()
+        self.port = checkEnvironmentCredentials.get_port()
+        self.database_name = checkEnvironmentCredentials.get_database_name()
